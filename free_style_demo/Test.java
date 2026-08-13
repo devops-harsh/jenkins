6d0272cbe1945c319f1ca10590a39e24e8de8054
HarshLKadam <devharsh2409@gmail.com>
2026-08-13 11:34:17 +0530

    declarative pipeline

---
+++ free_style_demo/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+
+        String name = "Harsh";
+        int age = 22;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+    }
+}
